aa3ed2685dbd1efa16aaa4359951d7a6bfbd554a
SkAsI7 <skasi.7@gmail.com>
2014-04-14 17:45:32 +0200

    Improved raw attribute fetching.

--- library/item.py
+++ library/item.py
@@ -1,8 +1,11 @@
 import json
 import library.common
+import re
 import urllib2
 
 class Item(object):
+    RegexObjectType = type(re.compile(''))
+
     def __init__(self, item_dict):
         object.__init__(self)
         self.__item_dict = item_dict
@@ -26,18 +29,29 @@ class Item(object):
     def get_raw_attributes(self):
         return self.__item_dict['attributesRaw']
 
-    def get_raw_attr(self, attr, exact=True):
-        if exact:
-            if attr in self.get_raw_attributes():
-                return (self.get_raw_attributes()[attr]['max'] + self.get_raw_attributes()[attr]['min']) / 2
-            else:
-                return 0
+    def get_raw_attribute(self, attribute):
+        if type(attribute) is self.RegexObjectType:
+            attributes = (a for n, a in self.get_raw_attributes().iteritems() if attribute.match(n))
         else:
-            acc = 0
-            for value in (x for x in self.get_raw_attributes().iterkeys() if attr in x):
-                acc += (value['max'] + value['min']) / 2
-            return acc
+            attributes = (a for n, a in self.get_raw_attributes().iteritems() if attribute == n)
+        result = 0.0
+        for attribute_ in attributes:
+            result += (attribute_['max'] + attribute_['min']) / 2
+        return result
 
     def __repr__(self):
         return '<%s>' % (self.get_name())
+
+
+if __name__ == '__main__':
+    item_dict = {'attributesRaw': {
+        'abc': {'min': 1, 'max': 1},
+        'abcd': {'min': 2, 'max': 2},
+        'abcde': {'min': 4, 'max': 4},
+    }}
+    item = Item(item_dict)
+    # Exact string
+    print item.get_raw_attribute('abcd')
+    # Regular expression
+    print item.get_raw_attribute(re.compile(r'abcd'))
 
